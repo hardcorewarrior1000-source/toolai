74b0b43c4cbcb2c6978d3837feda140db4c2b156
hardcorewarrior1000-source <hardcorewarrior1000@gmail.com>
2026-06-29 19:42:21 +0700

    Hardcode ExoClick banner ad in AdBanner component

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,17 +1,19 @@
+"use client";
+
+import Script from "next/script";
+
 export default function AdBanner() {
-  const adScript = process.env.NEXT_PUBLIC_AD_SCRIPT;
-
-  if (adScript) {
-    return (
-      <div className="w-full my-8" dangerouslySetInnerHTML={{ __html: adScript }} />
-    );
-  }
-
   return (
-    <div className="w-full bg-zinc-900/50 border border-zinc-800 rounded-lg p-4 text-center text-zinc-600 text-sm my-8">
-      <div className="h-24 flex items-center justify-center">
-        <span className="text-zinc-700">Ad Space</span>
-      </div>
+    <div className="w-full my-8">
+      <Script
+        async
+        src="https://a.magsrv.com/ad-provider.js"
+        strategy="afterInteractive"
+      />
+      <ins className="eas6a97888e2 block" data-zoneid="5961814" />
+      <Script id="exoclick-init" strategy="afterInteractive">
+        {`(AdProvider = window.AdProvider || []).push({"serve": {}});`}
+      </Script>
     </div>
   );
 }
